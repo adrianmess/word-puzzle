74840fc2a72992565be259ec125f6d3b7ab7c921
Adrian Messado <adrian.messado@gmail.com>
2018-03-27 22:14:47 -0700

    created function that will replace all vowels with dashes

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,11 @@
 $(document).ready(function(){
-  $("#formOne").submit(function(event){
+  $(".form").submit(function(event){
     event.preventDefault();
     var sentence = $("#inputSent").val();
-    // var result = sentence.removeWord('e');
-    // var wordArray = sentence.split(" ");
-     var res = sentence.split("a").join('-');
-    //  var resA= res.join("");
-     var resB = res.split("e").join('-');
-    //  var resC = resB.join("");
-     var resC = resB.split("i").join('-');
-    //  var resE = resD.join("");
-     var resD = resC.split("o").join('-');
-    //  var resG = resF.join("");
-     var resE = resD.split("u").join('-');
-    //  var resI = resH.join("");
-
-    console.log(resE);
-    // var res1 = res.split("e");
-    // var res2 = res1.split("i");
-    // console.log(res);
-
-
-    //  wordArray.forEach(function(word){
-    //    if
-    //  });
-    //
-    //
-    // = ["a", "e","i", "o", "u"];
-    // vowels.forEach(function(element) {
-    //
-    // vowels.
-    // var wordFuck = sentence.replace(vowels, "-");
-
-    // console.log(wordFuck);
-
-    // alert(wordFuck);
-    // ["a", "e","i", "o", "u"];
-    //
-    // wordFuck.forEach(function(vowel){
-    //   if(vowel)
-
-
+    var sentenceArray = sentence.split('');
+    for (var i = 0; i < sentenceArray.length; i++) {
+        sentenceArray[i] = sentenceArray[i].replace('e', '-').replace('a','-').replace('i','-').replace('o','-').replace('u','-')
+        };
+        newSentence = sentenceArray.join(' ');
   });
 });
