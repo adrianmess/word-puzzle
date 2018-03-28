309644c2460a98d24ad5933aa69c2ee7f64d2706
Adrian Messado <adrian.messado@gmail.com>
2018-03-27 22:22:32 -0700

    cleaned up indentation in scripts.js, added comment

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,24 @@ $(document).ready(function(){
         newSentence = sentenceArray.join(' ');
   });
 });
+
+
+
+
+//********* without for loop**********
+//
+// $(document).ready(function(){
+//   $("#formOne").submit(function(event){
+//     event.preventDefault();
+//     var sentence = $("#inputSent").val();
+//
+//     var res = sentence.split("a").join('-');
+//     var resB = res.split("e").join('-');
+//     var resC = resB.split("i").join('-');
+//     var resD = resC.split("o").join('-');
+//     var resE = resD.split("u").join('-');
+//
+//
+//     console.log(resE);
+//   });
+// });
