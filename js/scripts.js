cabc1b5d6f76a6d9d11187a2914513554701308c
Adrian Messado <adrian.messado@gmail.com>
2018-03-27 22:21:17 -0700

    cleaned up code in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,14 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     var sentence = $("#inputSent").val();
-    // var result = sentence.removeWord('e');
-    // var wordArray = sentence.split(" ");
-     var res = sentence.split("a").join('-');
-    //  var resA= res.join("");
-     var resB = res.split("e").join('-');
-    //  var resC = resB.join("");
-     var resC = resB.split("i").join('-');
-    //  var resE = resD.join("");
-     var resD = resC.split("o").join('-');
-    //  var resG = resF.join("");
-     var resE = resD.split("u").join('-');
-    //  var resI = resH.join("");
+
+    var res = sentence.split("a").join('-');
+    var resB = res.split("e").join('-');
+    var resC = resB.split("i").join('-');
+    var resD = resC.split("o").join('-');
+    var resE = resD.split("u").join('-');
+
 
     console.log(resE);
-    // var res1 = res.split("e");
-    // var res2 = res1.split("i");
-    // console.log(res);
-
-
-    //  wordArray.forEach(function(word){
-    //    if
-    //  });
-    //
-    //
-    // = ["a", "e","i", "o", "u"];
-    // vowels.forEach(function(element) {
-    //
-    // vowels.
-    // var wordFuck = sentence.replace(vowels, "-");
-
-    // console.log(wordFuck);
-
-    // alert(wordFuck);
-    // ["a", "e","i", "o", "u"];
-    //
-    // wordFuck.forEach(function(vowel){
-    //   if(vowel)
-
-
   });
 });
